80ef90a54f6a381eb5cdecfcc8b9d8f34a55b776
Tarunav BA <tarunavba@gmail.com>
2021-10-24 20:41:53 +0530

    👌 IMPROVE

--- src/toogabrowser/app.py
+++ src/toogabrowser/app.py
@@ -2,26 +2,53 @@
 A browser built using beeware's tooga module
 """
 import toga
-from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
+from toga.style.pack import CENTER, COLUMN, ROW, Pack
 
 
-class ToogaBrowser(toga.App):
-
+class Graze(toga.App):
     def startup(self):
-        """
-        Construct and show the Toga application.
+        self.main_window = toga.MainWindow(title=self.name)
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        self.webview = toga.WebView(style=Pack(flex=1))
+        self.url_input = toga.TextInput(
+            initial="https://github.com/TarunavBA", style=Pack(flex=1)
+        )
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        box = toga.Box(
+            children=[
+                toga.Box(
+                    children=[
+                        self.url_input,
+                        toga.Button(
+                            "Go",
+                            on_press=self.load_page,
+                            style=Pack(width=50, padding_left=5),
+                        ),
+                    ],
+                    style=Pack(
+                        direction=ROW,
+                        alignment=CENTER,
+                        padding=5,
+                    ),
+                ),
+                self.webview,
+            ],
+            style=Pack(direction=COLUMN),
+        )
+
+        self.main_window.content = box
+        self.webview.url = self.url_input.value
+
+        # Show the main window
         self.main_window.show()
+
+    def load_page(self, widget):
+        self.webview.url = self.url_input.value
 
 
 def main():
-    return ToogaBrowser()
+    return Graze("Graze", "org.beeware.graze")
+
+
+if __name__ == "__main__":
+    main().main_loop()
